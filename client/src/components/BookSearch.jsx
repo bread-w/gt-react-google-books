8b29d51d467b1b95f3ee19f89c8acbfd94512557
Brad Williams <jb.wms89@gmail.com>
2020-08-10 16:23:16 -0400

    created BookSearch jsx and now saving to create handle function

--- client/src/components/BookSearch.jsx
+++ client/src/components/BookSearch.jsx
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 
 class BookSearch extends Component {
+  state = {
+    books: [],
+    savedBooks: [],
+  };
+
+  handleBookSearch = (e) => {};
+
+  handleSavedBook = (e) => {};
+
   render() {
     return <div></div>;
   }
